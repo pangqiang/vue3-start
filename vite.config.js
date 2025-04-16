d025cb15eae835ff86161624bfb373ffb20ff69d
paidaxing <863199759@qq.com>
2025-04-17 00:46:34 +0800

    chore(vite): add alias for composables directory

--- vite.config.js
+++ vite.config.js
@@ -21,6 +21,7 @@ export default defineConfig({
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
             'components': fileURLToPath(new URL('./src/components', import.meta.url)),
+            'composables': fileURLToPath(new URL('./src/composables', import.meta.url)),
             'views': fileURLToPath(new URL('./src/views', import.meta.url)),
             'assets': fileURLToPath(new URL('./src/assets', import.meta.url))
         }
