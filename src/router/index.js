dae282ee56cebe7dc4182c987ffb7147c2fb79f5
paidaxing <863199759@qq.com>
2025-04-17 00:49:16 +0800

    chore: remove unused router

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import  page1Route from "@/router/page1.route";
-import  page2Route from "@/router/page2.route";
 const routes = [
     page1Route,
-    page2Route
 ]
 
 const router = createRouter({
