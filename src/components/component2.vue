1d02eea385d5357c074bdb5d58287f6f112b5b09
paidaxing <863199759@qq.com>
2025-04-17 00:47:26 +0800

    chore: remove unused  file

--- src/components/component2.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <h1>
-    组件示例2
-  </h1>
-</template>
-<script setup>
-</script>
--- src/router/page2.route.js
+++
@@ -1,7 +0,0 @@
-import component2 from "@/components/component2.vue";
-export default {
-    path: '/component2',
-    name: 'component2',
-    component: component2
-}
-
