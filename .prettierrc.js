f0a5405ec33d4fb259439d537b3442120cc03004
paidaxing <863199759@qq.com>
2025-04-22 21:48:06 +0800

    docs: Add configuration file comments

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+export default {
+  // 是否在语句末尾添加分号
+  semi: false,
+  // 是否使用单引号代替双引号
+  singleQuote: true,
+  // 每行的最大字符数
+  printWidth: 100,
+  // 缩进的空格数
+  tabWidth: 2,
+  // 是否在多行对象或数组的最后一项后添加逗号
+  trailingComma: 'none',
+  // 文件的换行符类型
+  endOfLine: 'lf'
+}
